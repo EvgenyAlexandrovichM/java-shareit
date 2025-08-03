09dca34b1e02caae7ccf3ecf42f8fb3b037db1e2
gfmx <smilevich@yandex.ru>
2025-08-03 17:57:40 +0300

    Удалены лишние проперти

--- gateway/src/main/java/ru/practicum/shareit/request/client/ItemRequestClient.java
+++ gateway/src/main/java/ru/practicum/shareit/request/client/ItemRequestClient.java
@@ -17,7 +17,7 @@ import java.util.Map;
 public class ItemRequestClient extends BaseClient {
     private static final String API_PREFIX = "/requests";
 
-    public ItemRequestClient(@Value("${shareit-server.url}") String serverUrl,
+    public ItemRequestClient(@Value("${shareit.server.url}") String serverUrl,
                              RestTemplateBuilder builder) {
         super(builder
                 .uriTemplateHandler(new DefaultUriBuilderFactory(serverUrl + API_PREFIX))
