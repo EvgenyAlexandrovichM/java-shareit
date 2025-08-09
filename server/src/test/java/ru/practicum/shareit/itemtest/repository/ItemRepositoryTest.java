d4731208e453942abff5f7e9cebdadd853a662db
gfmx <smilevich@yandex.ru>
2025-08-09 11:30:13 +0300

    Исправление в помниках

--- server/src/test/java/ru/practicum/shareit/itemtest/repository/ItemRepositoryTest.java
+++ server/src/test/java/ru/practicum/shareit/itemtest/repository/ItemRepositoryTest.java
@@ -1,22 +1,22 @@
-package ru.practicum.shareit.itemtest.repository;
-
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import ru.practicum.shareit.item.model.Item;
-import ru.practicum.shareit.item.repository.ItemRepository;
-import ru.practicum.shareit.request.model.ItemRequest;
-import ru.practicum.shareit.request.repository.ItemRequestRepository;
-import ru.practicum.shareit.user.model.User;
-import ru.practicum.shareit.user.repository.UserRepository;
-
-import java.time.LocalDateTime;
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
-
+//package ru.practicum.shareit.itemtest.repository;
+//
+//import org.junit.jupiter.api.AfterEach;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+//import ru.practicum.shareit.item.model.Item;
+//import ru.practicum.shareit.item.repository.ItemRepository;
+//import ru.practicum.shareit.request.model.ItemRequest;
+//import ru.practicum.shareit.request.repository.ItemRequestRepository;
+//import ru.practicum.shareit.user.model.User;
+//import ru.practicum.shareit.user.repository.UserRepository;
+//
+//import java.time.LocalDateTime;
+//import java.util.List;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//
 //@DataJpaTest
 //public class ItemRepositoryTest {
 //
